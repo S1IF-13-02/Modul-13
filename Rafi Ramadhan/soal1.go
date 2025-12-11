1550c64ee787a4ee414258bae3d455186087f1f6
rafiramadhan <rafirama1020@gmail.com>
2025-12-11 20:35:07 +0700

    Create soal1.go

---
+++ Rafi Ramadhan/soal1.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+    var n int
+    fmt.Print("Masukkan bilangan: ")
+    fmt.Scan(&n)
+
+    jumlahDigit := 0
+
+    for {
+        jumlahDigit++
+        n = n / 10
+
+        if n == 0 {
+            break
+        }
+    }
+
+    fmt.Println(jumlahDigit)
+}
