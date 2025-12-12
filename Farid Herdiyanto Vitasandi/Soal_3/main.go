36d0509512e14de4a4b5a51e4a14b824d6be6bb8
Ssandi <faridherdiyanto007@gmail.com>
2025-12-12 13:30:20 +0700

    Create main.go

---
+++ Farid Herdiyanto Vitasandi/Soal_3/main.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func main(){
+	var target_donasi int
+
+	fmt.Print("Masukkan target donasi: ")
+	fmt.Scan(&target_donasi)
+
+	total_donasi := 0
+	donatur := 0
+
+	for{
+		var donasi int
+		donatur++
+		fmt.Printf("Donatur ke-%d: Menyumbang ", donatur)
+		fmt.Scan(&donasi)
+
+		total_donasi += donasi
+
+		if total_donasi >= target_donasi{
+			break
+		}
+	}
+	fmt.Printf("Target donasi tercapai. Total donasi: %d dari %d donatur\n", total_donasi, donatur)
+}
