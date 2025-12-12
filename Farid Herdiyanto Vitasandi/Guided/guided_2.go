74342ced8351943ee8d2f9687e919bda3bb7430e
Ssandi <faridherdiyanto007@gmail.com>
2025-12-12 13:26:25 +0700

    Create guided_2.go

---
+++ Farid Herdiyanto Vitasandi/Guided/guided_2.go
@@ -0,0 +1,22 @@
+package main 
+
+import "fmt"
+
+func main(){
+
+	var bilangan int 
+	var continueLoop bool
+
+	
+	for continueLoop = true; continueLoop;{
+		fmt.Print("Masukkan bilangan bulat positif: ")
+		fmt.Scan(&bilangan)
+		continueLoop = bilangan <= 0
+
+		if bilangan <= 0{
+			fmt.Printf("%d bukan bilangan bulat positif. Silahkan coba lagi\n", bilangan)
+		}
+	}
+	fmt.Printf("%d adalah bilangan bulat positif", bilangan)
+}
+
