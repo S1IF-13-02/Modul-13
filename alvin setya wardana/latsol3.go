93761a2ba0075847f41d9a725d8143cfa5f2a0cd
alvin09-a11y <celinecristina838@gmail.com>
2025-12-16 17:09:07 +0700

    Add donation tracking program in latsol3.go

---
+++ alvin setya wardana/latsol3.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+func main() {
+	var target int
+	var donasi int
+	var total int
+	var jumlahDonatur int
+	var selesai bool
+
+	fmt.Scan(&target)
+
+	total = 0
+	jumlahDonatur = 0
+
+	for selesai = false; !selesai; {
+		fmt.Scan(&donasi)
+		jumlahDonatur++
+		total = total + donasi
+
+		fmt.Printf(
+			"Donatur %d : Menyumbang %d. Total terkumpul: %d\n",
+			jumlahDonatur, donasi, total,
+		)
+
+		selesai = total >= target
+	}
+
+	fmt.Printf(
+		"Target tercapai! Total donasi: %d dari %d donatur.\n",
+		total, jumlahDonatur,
+	)
+}
